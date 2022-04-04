a43da2a2b6e993364f7c778814f6414abff51f8a
m6mok <dea2003@bk.ru>
2022-04-04 22:11:02 +0300

    Added patterns

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('groups/<int:num>/', views.group_posts),
+    path('', views.index, name='main'),
+    path('groups/<int:num>/', views.group_posts, name='group'),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<p>Hello, World!</p>')
+    return render(request, 'posts/index.html')
 
 
 def group_posts(request, num):
-    return HttpResponse(f'This is {num}')
+    return render(request, 'posts/groups.html')
