3107737d9684dab7f9d262ee3bb85ae1f17c8239
m6mok <dea2003@bk.ru>
2022-04-04 21:26:31 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse('<p>Hello, World!</p>')
+
+
+def group_posts(request, num):
+    return HttpResponse(f'This is {num}')
